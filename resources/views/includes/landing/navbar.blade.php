0dc37a1d00a401e9b2b68e9d2268466950e8ae02
Septian Padli <muhammadpadliseptiana@gmail.com>
2023-03-08 10:29:52 +0700

    Initial commit

---
+++ resources/views/includes/landing/navbar.blade.php
@@ -0,0 +1,26 @@
+<!-- ======= Header ======= -->
+<header id="header" class="fixed-top d-flex align-items-center" style="background-color: #FF7F50
+;">
+    <div class="container d-flex align-items-center justify-content-between">
+
+      <div class="logo">
+        <h1>
+            <a class="navbar-brand fw-bold text-white" href="{{ url('/')}}">
+                <img src="/assets/img/logo-1@4x.png" alt="Logo" class="img-fluid">
+                E-Ng4duu.COM
+            </a>
+        </h1>
+
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <div> <a class="border-solid" href="{{ url('login')}}">Masuk</a></div>
+          <div> <a class="border-solid" href="{{ url('register')}}">Daftar</a></div>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header><!-- End Header -->
